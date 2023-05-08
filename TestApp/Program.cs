d1ce064f7635a19cdb50b71fa7adefb97f05123f
Farouk <Farouk@DESKTOP-VOK1F94>
2023-05-08 14:50:43 -0700

    Add project files.

---
+++ TestApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TestApp
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
